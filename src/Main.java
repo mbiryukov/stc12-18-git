abfe5c7ac44ac2d3cc5e74de55948462bfb9bd7c
MVBiryukov-stc11 <m.biryukov@innopolis.ru>
2018-09-26 20:15:06 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Some code");
         System.out.println("Some more code");
+        System.out.println("Third string");
     }
 
 }
